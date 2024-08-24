871fdbecb30bfc886e13c3ca992d87936138d36d
jinhwan7 <rkflql851@gmail.com>
2024-08-24 13:37:02 +0900

    first

---
+++ src/main/java/com/example/subjectproblem/SubjectproblemApplication.java
@@ -0,0 +1,15 @@
+package com.example.subjectproblem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectproblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubjectproblemApplication.class, args);
+
+
+	}
+
+}
